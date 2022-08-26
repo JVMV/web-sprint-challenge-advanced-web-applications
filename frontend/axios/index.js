114da6bde1d35d6aa7ec74f7a95ffa2c73ef404a
Justin Abellera <justin.c.abellera@gmail.com>
2022-08-26 15:23:10 -0700

    app progress

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,12 @@
 // ✨ implement axiosWithAuth
+import axios from 'axios'
+
+export const axiosWithAuth = () => {
+    axios.create({
+        baseURL: 'http://localhost:9000/api/',
+        headers: {
+            Authorization: token
+        }
+    })
+}
+
